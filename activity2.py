65713bc9b6aa9d33d0f1a4e789dcc35e912a2c9c
TSathvik189 <sathvikreddy5678@gmail.com>
2024-10-04 18:32:52 +0530

    Update activity2.py

--- activity2.py
+++ activity2.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World Im Sathvik'
+    return 'Hello World Im Sathvik my tutor is Nandhini'
 
 # main driver function
 if __name__ == '__main__':
